6cc9e29be9aebfd816df3e37b9fbca3bd1b4ebc4
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2010-12-16 13:18:08 +0100

    made context path of the server configurable, code cleanup

--- README.txt
+++ README.txt
@@ -6,15 +6,23 @@ of exams.
 
 It was developed at the University of Leipzig.
 
+
+Configuration:
+==============
 This server gets configured by the settings in sslsettings.properties. Per default the server runs an unencrypted
 http connector at port 8080. For securing the server please create a java keystore that contains a valid certificate 
 (including a RSA keypair) as well as at least one public key (for client authentication).
 Please use the provided scripts (createCertificates.(sh|bat)) for generating self-signed certificates for test setups. Please change the password setting
 accordingly.
 
+All application data gets written to a directory located at {user.home}/ExamServerRepository_{name} where 
+name is either "webapp" or the value of the variable "elatexam.name" in the configuration file 
+sslsettings.properties. This is especially useful if you would like to run multiple instances of elateXam 
+in parallel. In that case you would need to set the variable to a unique name before starting each instance. 
+
+
 Running:
 =========
-
 There are two modes: Test run with an unsecured http connector OR completely secure SSL communication with 
 client certificates (unsecured access only allowed from localhost).
 
--- src/main/java/de/elatexam/embedded/Startup.java
+++ src/main/java/de/elatexam/embedded/Startup.java
@@ -19,9 +19,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 package de.elatexam.embedded;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,7 +29,6 @@ import javax.management.MBeanServer;
 
 import org.mortbay.jetty.Connector;
 import org.mortbay.jetty.Server;
-import org.mortbay.jetty.deployer.WebAppDeployer;
 import org.mortbay.jetty.nio.SelectChannelConnector;
 import org.mortbay.jetty.security.SslSocketConnector;
 import org.mortbay.jetty.webapp.WebAppContext;
@@ -43,84 +40,103 @@ import org.mortbay.management.MBeanContainer;
  */
 public class Startup {
 
-    public Startup() throws FileNotFoundException, IOException {
+  public Startup() throws Exception {
 
-        Properties prop = new Properties();
-        prop.load(new FileReader("sslsettings.properties"));
+    Properties prop = new Properties();
+    prop.load(new FileReader("sslsettings.properties"));
 
-        try {
-            Server server = new Server();
+    final int httpPort = Integer.parseInt(prop.getProperty("port.http", "8080"));
+    Server server = initializeServer(prop, httpPort);
+    initializeWebapps(prop, server);
+    startJMX(server);
+    server.start();
 
-            // if (!contextPath.equals("/")) {
-            // server.addHandler(new MovedContextHandler(server, "/", contextPath));
-            // }
+    System.out.println("###########################################################");
+    System.out.println("#");
+    System.out.printf("#  Ready at http://localhost%s/%s\n", (httpPort != 0 ? ":" + httpPort : ""), prop.getProperty("elatexam.name", "examServer"));
+    System.out.println("#");
+    System.out.println("###########################################################");
+    server.join();
+  }
 
-            List<Connector> conns = new ArrayList<Connector>(2);
+  /**
+   * @param prop
+   * @param httpPort
+   * @return
+   * @throws Exception
+   */
+  protected Server initializeServer(Properties prop, int httpPort) throws Exception {
+    Server server = new Server();
 
-            String httpPortString = prop.getProperty("port.http", "8080");
+    // if (!contextPath.equals("/")) {
+    // server.addHandler(new MovedContextHandler(server, "/", contextPath));
+    // }
 
-            int httpPort = Integer.parseInt(httpPortString);
+    List<Connector> conns = new ArrayList<Connector>(2);
 
-            SelectChannelConnector httpConn = new SelectChannelConnector();
-            httpConn.setPort(httpPort);
-            // allow access to the unsecured connector from localhost only!
-            conns.add(httpConn);
-
-            server.setConnectors(conns.toArray(new Connector[conns.size()]));
-            server.setStopAtShutdown(true);
-
-            final WebAppDeployer wad = new WebAppDeployer();
-            wad.setExtract(true);
-            wad.setContexts(server);
-            wad.setWebAppDir(prop.getProperty("war.directory", "wars"));
-            // wad.setExtract(true);
-            wad.start();
-            // use empty session path to make sure, all webapps share the session id
-            // this is needed for data exchange via TaskModelViewDelegate
-            for (org.mortbay.jetty.Handler handler : server.getHandlers())
-                if (handler instanceof WebAppContext) {
-                    WebAppContext context = ((WebAppContext) handler);
-                    context.getSessionHandler().getSessionManager().setSessionPath("/");
-                }
-
-            if (addSSLConnector(server, prop) == true) {
-                // whenn ssl is enabled disallow non-ssl access from all ips but localhost
-                httpConn.setHost("localhost");
-            }
-            startJMX(server);
-            server.start();
-
-            if (httpPort != 0) {
-                System.out.println("Ready at http://localhost:" + httpPort);
-            }
-            server.join();
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
+    SelectChannelConnector httpConn = new SelectChannelConnector();
+    httpConn.setPort(httpPort);
+    // allow access to the unsecured connector from localhost only!
+    conns.add(httpConn);
+    if (addSSLConnector(server, prop) == true) {
+      // whenn ssl is enabled disallow non-ssl access from all ips but localhost
+      httpConn.setHost("localhost");
     }
 
-    private boolean addSSLConnector(Server server, Properties prop) throws Exception {
-        final String keystoreFile = prop.getProperty("keystore.filename", "server.keystore");
-        final String password = prop.getProperty("keystore.password", "testtest");
-        String httpsPort = prop.getProperty("port.https", "8443");
+    server.setConnectors(conns.toArray(new Connector[conns.size()]));
+    server.setStopAtShutdown(true);
+    return server;
+  }
 
-        if (new File(keystoreFile).exists()) {
-            SslSocketConnector ssl = new SslSocketConnector();
-            ssl.setPort(Integer.parseInt(httpsPort));
-            ssl.setKeystore(keystoreFile);
-            ssl.setPassword(password);
-            ssl.setKeyPassword(password);
+  /**
+   * @param prop
+   * @param elatexamContextPath
+   * @param server
+   * @throws Exception
+   */
+  protected void initializeWebapps(Properties prop, Server server) throws Exception {
+    // set name of the elatexam context path
+    final String elatexamContextPath = prop.getProperty("elatexam.name", "examServer");
+    System.setProperty("elatexam.name", elatexamContextPath);
 
-            ssl.setNeedClientAuth(true);
-            ssl.setTruststore(keystoreFile);
-            ssl.setTrustPassword(password);
+    // initialize taskmodel-core-view webapp
+    WebAppContext tcv = new WebAppContext(prop.getProperty("war.directory", "wars") + "/taskmodel-core-view.war", "/taskmodel-core-view");
+    tcv.setExtractWAR(true);
+    server.addHandler(tcv);
+    // initialize examserver webapp, use configured context path
+    WebAppContext scv = new WebAppContext(prop.getProperty("war.directory", "wars") + "/examServer.war", "/"
+        + elatexamContextPath);
+    scv.setExtractWAR(true);
+    server.addHandler(scv);
+    // use empty session path to make sure, all webapps share the session id
+    // this is needed for data exchange via TaskModelViewDelegate
+    for (org.mortbay.jetty.Handler handler : server.getHandlers())
+      if (handler instanceof WebAppContext) {
+        WebAppContext context = ((WebAppContext) handler);
+        context.getSessionHandler().getSessionManager().setSessionPath("/");
+      }
+  }
 
-            server.addConnector(ssl);
-            return true;
-        } else {
-            System.out.println("\n" +
+  private boolean addSSLConnector(Server server, Properties prop) throws Exception {
+    final String keystoreFile = prop.getProperty("keystore.filename", "server.keystore");
+    final String password = prop.getProperty("keystore.password", "testtest");
+    String httpsPort = prop.getProperty("port.https", "8443");
+
+    if (new File(keystoreFile).exists()) {
+      SslSocketConnector ssl = new SslSocketConnector();
+      ssl.setPort(Integer.parseInt(httpsPort));
+      ssl.setKeystore(keystoreFile);
+      ssl.setPassword(password);
+      ssl.setKeyPassword(password);
+
+      ssl.setNeedClientAuth(true);
+      ssl.setTruststore(keystoreFile);
+      ssl.setTrustPassword(password);
+
+      server.addConnector(ssl);
+      return true;
+    } else {
+      System.out.println("\n" +
                     "#######################################################################\n" +
                     "# There seems to be no keystore available. Please generate one using \n" +
                     "#  for Linux: createCertificates.sh \n" +
@@ -130,27 +146,26 @@ public class Startup {
                     "# WARNING: SSL will not be available! \n" +
                     "# \n" +
                     "#######################################################################");
-            return false;
-        }
-
+      return false;
     }
 
-    private void startJMX(Server server) {
-        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
-        MBeanContainer mBeanContainer = new MBeanContainer(mBeanServer);
-        server.getContainer().addEventListener(mBeanContainer);
-        mBeanContainer.start();
+  }
 
-    }
+  private void startJMX(Server server) {
+    MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+    MBeanContainer mBeanContainer = new MBeanContainer(mBeanServer);
+    server.getContainer().addEventListener(mBeanContainer);
+    mBeanContainer.start();
 
-    /**
-     * @param args
-     * @throws IOException
-     * @throws FileNotFoundException
-     */
-    public static void main(final String[] args) throws FileNotFoundException, IOException {
-        new Startup();
+  }
 
-    }
+  /**
+   * @param args
+   * @throws Exception
+   */
+  public static void main(final String[] args) throws Exception {
+    new Startup();
+
+  }
 
 }
