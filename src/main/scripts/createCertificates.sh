23b52051696e2685410dd6f4de2f94c601fa9a1e
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2011-06-14 10:38:58 +0200

    [fix] do not expect the keytool to be available in the PATH

--- src/main/scripts/createCertificates.sh
+++ src/main/scripts/createCertificates.sh
@@ -30,21 +30,21 @@ if [ -r "$JAVA_HOME"/bin/keytool ]; then
 	clientalias="clientCert"
 	
 	echo "Generating new server certificate key..."
-	keytool -genkeypair -alias ${serveralias} -keyalg RSA -validity 365 -keystore server.keystore -dname cn="${commonname}",o="${ownername}",l="${location}" -keypass ${password} -storepass ${password}
+	"$JAVA_HOME"/bin/keytool -genkeypair -alias ${serveralias} -keyalg RSA -validity 1000 -keystore server.keystore -dname cn="${commonname}",o="${ownername}",l="${location}" -keypass ${password} -storepass ${password}
 	
 	# wait for app. 1 second to prevent generation of two certificates with the same serial number
 	sleep 1
 	
 	echo "Generating new client certificate key..."
-	keytool -genkeypair -alias ${clientalias} -keyalg RSA -validity 365 -storetype pkcs12 -keystore clientcertificate.p12 -dname cn="${commonname}",o="${ownername}",l="${location}"  -storepass ${password} -keypass ${password}
+	"$JAVA_HOME"/bin/keytool -genkeypair -alias ${clientalias} -keyalg RSA -validity 1000 -storetype pkcs12 -keystore clientcertificate.p12 -dname cn="${commonname}",o="${ownername}",l="${location}"  -storepass ${password} -keypass ${password}
 	
 	# export certificates
 	echo "Importing public client certificate into server keystore..."
-	keytool -exportcert -alias ${clientalias} -keystore clientcertificate.p12 -storetype pkcs12 -storepass ${password} -file client-public.cer
-	keytool -importcert -v -alias ${clientalias} -file client-public.cer -keystore server.keystore -storepass ${password} -noprompt -trustcacerts
+	"$JAVA_HOME"/bin/keytool -exportcert -alias ${clientalias} -keystore clientcertificate.p12 -storetype pkcs12 -storepass ${password} -file client-public.cer
+	"$JAVA_HOME"/bin/keytool -importcert -v -alias ${clientalias} -file client-public.cer -keystore server.keystore -storepass ${password} -noprompt -trustcacerts
 	
 	rm client-public.cer
-	keytool -list -v -keystore server.keystore -storepass ${password}
+	"$JAVA_HOME"/bin/keytool -list -v -keystore server.keystore -storepass ${password}
 	echo "Please import clientcertificate.p12 into your browser, use server.keystore as keystore/truststore in tomcat 5.5."
 	echo "For details on configuring tomcat please refer to http://tomcat.apache.org/tomcat-5.5-doc/ssl-howto.html"
 	echo "Done!"
